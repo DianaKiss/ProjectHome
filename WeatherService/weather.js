d8d87d442597cd483e5908bd91135c551c347f35
DianaKiss <dana.butcher@gmail.com>
2018-10-15 19:50:05 +0200

    Weather service not working yet

--- WeatherService/weather.js
+++ WeatherService/weather.js
@@ -1,12 +1,31 @@
 'use strict';
 
 const newRequest = new XMLHttpRequest();
+let input = document.querySelector('.search');
 
-newRequest.open('GET', 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', true);
+input.addEventListener('submit', (event) => {
+  const locationSearch = event.target.elements.locationSearch.value;
 
-newRequest.onload = () => {
-  let response = newRequest.responseText;
-  console.log(response);
-}
+  newRequest.open('GET', `https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${locationSearch}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`, true);
 
-newRequest.send();
+  newRequest.onload = () => {
+    let response = newRequest.responseText;
+    console.log(response);
+    //   document.createElement('div').innerHTML = `<div class="tile">
+    //   <div class="location">
+    //     <p class="city">${locationSearch}</p>
+    //     <p class="county">${locationSearch}</p>
+    //   </div>
+    //   <p class="temperature">${response.temp}</p>
+    //   <p class="terms">${response.text}</p>
+    // </div>`
+    }
+
+    newRequest.send();
+
+
+event.preventDefault;
+
+
+  });
+
